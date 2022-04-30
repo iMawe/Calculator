a3149ad77d601fcd09bfff1a9ac8416d2015ecfe
iMawe <37808370+iMawe@users.noreply.github.com>
2022-04-30 12:01:15 -0500

    Multiplicacion v1
    Se crea la funcion multipicacion para la calculadora

---
+++ ModMult.py
@@ -0,0 +1,14 @@
+def mult(num1, num2):
+    return num1 * num2
+  
+print("Multiply")
+  
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+  
+print(number_1, "*", number_2, "=",
+                mult(number_1, number_2))
+  
+
+
+  
